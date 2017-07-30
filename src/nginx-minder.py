054d724e5c5ab7acaed9c2d6eb5a55aae4bb9561
Philip Branning <branning@gmail.com>
2017-07-30 00:02:46 +0000

    generator function to follow a file

--- src/nginx-minder.py
+++ src/nginx-minder.py
@@ -14,7 +14,20 @@ Options:
     --version        Show version
 '''
 
+from time import sleep
+
 from docopt import docopt
+
+def follow(filename):
+    '''open a file and jump to the end, then yield all the lines that show up'''
+    with open(filename, 'r') as f:
+        f.seek(0, 2)  # go to the end
+        while True:
+            line = f.readline()
+            if not line:
+                sleep(0.1) # don't spin-wait
+                continue
+            yield line
 
 if __name__=='__main__':
     arguments = docopt(__doc__, version='nginx-minder 1.0')
