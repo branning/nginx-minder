392e0eddd94d9ebe43ccebb1aa5768a6efe8c94e
Philip Branning <branning@gmail.com>
2017-07-31 19:43:05 +0000

    mount logfile into both containers as volumex

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 #
 # TODO: use docker compose instead of this
 
+here=$(readlink -f $0 || $0 && pwd)
+
 make nginx
 make nginx-minder
-docker run -d -p 80:80 custom-nginx
-docker run -d nginx-minder
+docker run -d -p 80:80 -v "$here/nginx-logs":/var/log/nginx branning/nginx-to-mind
+docker run -d -v "$here/nginx-logs":/var/log/nginx branning/nginx-minder
